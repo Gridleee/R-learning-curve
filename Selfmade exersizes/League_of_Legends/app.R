d053f8317facf7ab2e46bb1d923b813cc6d57c6b
Karlijn Onrust <konrust@platogroup.eu>
2018-11-23 16:33:52 +0100

    End day 23-11-18

--- Selfmade exersizes/League_of_Legends/app.R
+++ Selfmade exersizes/League_of_Legends/app.R
@@ -1,3 +1,25 @@
+###Compare stats
+##Visual
+#TODO: lists in ABC.
+#TODO: search on champs
+#TODO: when drilling when filtering
+#TODO: Navbar css
+
+##Table
+#TODO: Table on filter from visual
+
+
+###Champs stats
+#choose champ
+#choose level
+#titel = selected champ
+#autoselected champ = AAA
+#star with stats
+
+###about this
+#text
+
+
 
 library(shiny)
 library(readxl)
@@ -5,11 +27,10 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-Base_stats <- read_excel("D:/R-learning-curve/Selfmade exersizes/All Champions Stats.xlsx", sheet = "Base Stats")
+Champs <- read_excel("D:/R-learning-curve/Selfmade exersizes/All Champions Stats.xlsx", sheet = "NAME")
 Types <- read_excel("D:/R-learning-curve/Selfmade exersizes/All Champions Stats.xlsx", sheet = "TYPES")
 Primary <- read_excel("D:/R-learning-curve/Selfmade exersizes/All Champions Stats.xlsx", sheet = "PRIMARY")
 
-attach(Base_stats)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -19,7 +40,7 @@ ui <- fluidPage(
   navbarPage("League of Legends, Raw stats",
     tabPanel("Compare stats",
       sidebarLayout(
-        sidebarPanel(
+        sidebarPanel(class = "nav",
           sliderInput("level", "Level", 1, 18, 1),
           tags$table(
             tags$tr(
@@ -31,35 +52,24 @@ ui <- fluidPage(
               )
             )
           ),
-          
           tags$table(
             tags$tr(
-              tags$th(class = "marg",
+              tags$th(
+                tags$div( class = "champscroll", checkboxGroupInput("Champs", "Champion", Champs$NAME))
+              ),
+              tags$th(
+                checkboxGroupInput("Primary", "Primary", Primary$PRIMARY)
+              ),
+              tags$th(
                 checkboxGroupInput("Type", "Type", Types$TYPE)
-              ),
-              tags$th(class = "marg",
-                tags$p("#van dit type")
-                
-              )
-            ),
-            tags$tr(
-              tags$th(class = "marg",
-                checkboxGroupInput("Type", "Type", Primary$PRIMARY)
-              ),
-              tags$th(class = "marg",
-                tags$p("#van deze primary")
-              )
+                )
             )
-          ),
-          tags$div( class = "champscroll",
-            checkboxGroupInput("champs", "Champion", Base_stats$NAME)
           )
         ),
         mainPanel(
           tabsetPanel(
             tabPanel("Visual", br(),
-              plotOutput("stats_v"),
-              textOutput("error")
+              plotOutput("stats_v", height = 700)
             ),
             tabPanel("Table", br(),
               tableOutput("stats_t")
@@ -87,6 +97,9 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  Base_stats <- read_excel("D:/R-learning-curve/Selfmade exersizes/All Champions Stats.xlsx", sheet = "Base Stats")
+ 
   lv <- reactive({
     input$level
   })
@@ -99,14 +112,51 @@ server <- function(input, output) {
     paste(input$y_as, "PLUS", sep="")
   }) 
   
-  output$stats_v <- renderPlot({
-    ggplot(Base_stats, aes(Base_stats[[input$x_as]] + Base_stats[[plusx()]] * (lv() - 1), Base_stats[[input$y_as]] + Base_stats[[plusy()]] * (lv() - 1), colour=NAME, label = NAME)) + geom_point(size=2) + geom_text(hjust = -0.1) + theme(legend.position="none") + labs(y = input$y_as, x = input$x_as)   
-  })
+  filtered <- NULL
   
-  output$error <- renderText({
+  as.data.frame(filtered)
+  
+  output$stats_v <- renderPlot({
+    
+    if(is.null(input$Type)){
+      if(is.null(input$Primary)){
+        filtered <- Base_stats
+      }
+    }
+    
+    if(!is.null(input$Type)){
+      i = as.list(input$Type) 
+      filtered = NULL
+      as.data.frame(filtered)
+      while(length(i) > 0){
+        filtered <- rbind(filtered, filter(Base_stats, Base_stats$TYPE == i[1]))
+        i[1] = NULL
+      }
+    }
+    
+    if(!is.null(input$Primary)){
+      i = as.list(input$Primary)
+      filtered = NULL
+      as.data.frame(filtered)
+      while(length(i) > 0){
+        filtered <- rbind(filtered, filter(Base_stats, Base_stats$PRIMARY == i[1]))
+        i[1] = NULL
+      }
+    }
+    
+    if(!is.null(input$Champs)){
+      i = as.list(input$Champs)
+      filtered = NULL
+      as.data.frame(filtered)
+      while(length(i) > 0){
+        filtered <- rbind(filtered, filter(Base_stats, Base_stats$NAME == i[1]))
+        i[1] = NULL
+      }
+    }
+    
+    ggplot(filtered, aes(filtered[[input$x_as]] + filtered[[plusx()]] * (lv() - 1), filtered[[input$y_as]] + filtered[[plusy()]] * (lv() - 1), colour=NAME, label = NAME)) + geom_point(size=2) + geom_text(hjust = -0.1) + theme(legend.position="none") + labs(y = input$y_as, x = input$x_as)   
     
   })
-  
 }
 
 # Run the application 
